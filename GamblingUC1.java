f7aac3ff3ff787ad03ea349bb0c67c84271146ef
Anushanallap <anusha.n468@gmail.com>
2021-12-13 11:43:39 +0530

    add file

---
+++ GamblingUC1.java
@@ -0,0 +1,15 @@
+public class GamblingUc1
+{
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome Gambler");
+
+        int Every_Day_Stack=100;
+        int Bet_Every_Game=1;
+
+        System.out.println("Every Day Stack: "+Every_Day_Stack);
+        System.out.println("Bet Price: "+Bet_Every_Game);
+
+    }
+}
